644737c87e7258093a4c32a130c7455c2b66785f
Michal <kwiatkowski.michal.2003@gmail.com>
2025-09-21 18:27:59 +0200

    bielik update

--- benchmark_framework/models/bielik.py
+++ benchmark_framework/models/bielik.py
@@ -7,19 +7,12 @@ from benchmark_framework.constants import SYSTEM_PROMPT
 class BielikModel(BaseModel):
     """
     Bielik language model implementation from Hugging Face.
-
-    This class handles loading the model and tokenizer, and then
-    uses them to generate responses for given prompts.
     """
 
     def __init__(self, model_name: str, **kwargs):
         super().__init__(model_name, **kwargs)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-
-        print("Loading tokenizer...")
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-
-        print("Loading model...")
         self.model = AutoModelForCausalLM.from_pretrained(
             self.model_name,
             torch_dtype=torch.bfloat16,
@@ -29,21 +22,20 @@ class BielikModel(BaseModel):
 
     def generate_response(self, prompt: str) -> str:
         """
-        Generate an answer for a multiple-choice question using the Bielik model.
-
-        Args:
-            prompt: The question prompt (with question and choices).
-
-        Returns:
-            A string containing the model’s response.
+        Generate an answer for the given prompt using the Bielik model.
         """
         messages = [
             {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": prompt}
         ]
-        input_ids = self.tokenizer.apply_chat_template(messages, return_tensors="pt").to(self.device)
-        outputs = self.model.generate(input_ids, max_new_tokens=1000)
-        response = self.tokenizer.batch_decode(outputs)[0]
-        print(response)
+        inputs = self.tokenizer.apply_chat_template(messages, return_tensors="pt", return_attention_mask=True,
+                                                    return_dict=True)
+        input_ids = inputs["input_ids"].to(self.device)
+        input_len = input_ids.shape[1]
+
+        attention_mask = inputs["attention_mask"].to(self.device)
+        outputs = self.model.generate(input_ids=input_ids, attention_mask=attention_mask, max_new_tokens=1000)
+        generated_tokens = outputs[0, input_len:]
+        response = self.tokenizer.decode(generated_tokens, skip_special_tokens=True)
 
         return response
