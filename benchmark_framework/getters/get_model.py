72cdbc7b7937fee171c92df53ba39768105c9a91
Michal <kwiatkowski.michal.2003@gmail.com>
2025-10-13 10:56:57 +0200

    get_model_type bugfix

--- benchmark_framework/getters/get_model.py
+++ benchmark_framework/getters/get_model.py
@@ -16,7 +16,7 @@ def _get_model_type(model_name: str) -> str:
     """
     Get the model type from the model name (e.g. gemini, speakleaash, CYFRAGOVPL)
     """
-    model_name_split = re.split(r"[, -]+", model_name)
+    model_name_split = re.split(r"[/ -]+", model_name)
     assert len(model_name_split) >= 1
     return model_name_split[0]
 
@@ -26,7 +26,6 @@ def get_model_by_name(model_name, model_config: ModelConfig) -> BaseModel:
     Factory function to get a model instance by name.
     """
     model_type = _get_model_type(model_name)
-    print(model_type)
     model_class = MODEL_REGISTRY.get(model_type)
     if not model_class:
         raise ValueError(f"Model name '{model_name}' is not recognized.")
