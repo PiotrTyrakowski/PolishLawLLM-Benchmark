69e3b9c3b1dd6c61b3d42aa1285513db7c6ead56
Michal <kwiatkowski.michal.2003@gmail.com>
2025-10-13 10:49:22 +0200

    get model update

--- benchmark_framework/getters/get_model.py
+++ benchmark_framework/getters/get_model.py
@@ -1,22 +1,31 @@
+import re
+
 from benchmark_framework.models.base_model import BaseModel
 from benchmark_framework.models.gemini import GeminiModel
 from benchmark_framework.models.local_model import LocalModel
 from benchmark_framework.configs.model_config import ModelConfig
 
 MODEL_REGISTRY = {
-    "gemini-2.5-pro": GeminiModel,
-    "gemini-2.5-flash": GeminiModel,
-    "gemini-2.5-flash-lite": GeminiModel,
-    "speakleash/Bielik-4.5B-v3.0-Instruct": LocalModel,
-    "CYFRAGOVPL/Llama-PLLuM-8B-chat": LocalModel,
+    "gemini": GeminiModel,
+    "speakleash": LocalModel,
+    "CYFRAGOVPL": LocalModel,
 }
+
+
+def _get_model_type(model_name: str) -> str:
+    """
+    Get the model type from the model name (e.g. gemini, speakleaash, CYFRAGOVPL)
+    """
+    model_name_split = re.split(r"[, -]+", model_name)
+    assert len(model_name_split) >= 1
+    return model_name_split[0]
 
 
 def get_model_by_name(model_name, model_config: ModelConfig) -> BaseModel:
     """
     Factory function to get a model instance by name.
     """
-    model_class = MODEL_REGISTRY.get(model_name)
+    model_class = MODEL_REGISTRY.get(_get_model_type(model_name))
     if not model_class:
         raise ValueError(f"Model name '{model_name}' is not recognized.")
     model_instance = model_class(model_name, model_config)
