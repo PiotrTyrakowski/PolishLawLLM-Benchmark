0734e935fe989f8af0048c411dcbf3f7d8d2d606
Michal <kwiatkowski.michal.2003@gmail.com>
2025-10-13 10:55:09 +0200

    debug log

--- benchmark_framework/getters/get_model.py
+++ benchmark_framework/getters/get_model.py
@@ -25,7 +25,9 @@ def get_model_by_name(model_name, model_config: ModelConfig) -> BaseModel:
     """
     Factory function to get a model instance by name.
     """
-    model_class = MODEL_REGISTRY.get(_get_model_type(model_name))
+    model_type = _get_model_type(model_name)
+    print(model_type)
+    model_class = MODEL_REGISTRY.get(model_type)
     if not model_class:
         raise ValueError(f"Model name '{model_name}' is not recognized.")
     model_instance = model_class(model_name, model_config)
