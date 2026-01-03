a7418ce505c46df9297ef2fe95133b107b60a5f3
Michal <kwiatkowski.michal.2003@gmail.com>
2026-01-03 11:40:48 +0100

    date in exam manager bugfix

--- src/benchmark_framework/managers/exam_manager.py
+++ src/benchmark_framework/managers/exam_manager.py
@@ -11,7 +11,7 @@ from src.benchmark_framework.utils.response_parser import extract_json_field
 EXACT_DATE_DICT: dict[int, str] = {
     2025: "17 marca 2025",
     2024: "29 marca 2024",
-    2023: "23 marca 2024",
+    2023: "23 marca 2023",
     2022: "16 marca 2022",
     2021: "25 marca 2021",
     2020: "10 kwietnia 2020",
