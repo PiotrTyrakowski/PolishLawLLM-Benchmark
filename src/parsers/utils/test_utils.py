03fa575f6235e257a53c0225cab8d940831818a3
Michal <kwiatkowski.michal.2003@gmail.com>
2026-01-24 17:32:03 +0100

    parsers test utils update

--- src/parsers/utils/test_utils.py
+++ src/parsers/utils/test_utils.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def get_data_path(*path_parts: str) -> Path:
     # Start from this file and go up to the project root
-    file_path = Path(__file__).parent / ".." / ".." / "data"
+    file_path = Path(__file__).parent / ".." / ".." / ".." / "data"
     for part in path_parts:
         file_path = file_path / part
     return file_path.resolve()
